8ca6bbf88c71e67bdf5b5e76a57ed4b49b520807
Cameron Aragon <caragon4695@gmail.com>
2021-02-17 09:36:10 -0700

    Build out base of the Card component

---
+++ src/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+    return (
+        <div>
+            <h2>Name</h2>
+        </div>
+    )
+}
+
+export default Card;
--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Reservations.css';
+import Card from '../Card/Card';
 
 const Reservations = () => {
     return (
         <section className='resy-container'>
             <h1>Hello</h1>
+            <Card />
         </section>
     )
 }
