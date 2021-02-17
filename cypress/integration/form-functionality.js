1afb9b58b857462368fab11c5f374c5f9cb776cb
Cameron Aragon <caragon4695@gmail.com>
2021-02-17 11:57:35 -0700

    Finish form functionality testing

--- cypress/integration/form-functionality.js
+++ cypress/integration/form-functionality.js
@@ -14,7 +14,64 @@ describe('Reservation Display', () => {
     it('should have a form that takes in text on the form inputs', () => {
         cy
         .get('.resy-form').within(() => {
-            cy.get('input:first').type('Cameron')
+            cy.get('.name-input').type('Cameron')
+            cy.get('.date-input').type('01/14')
+            cy.get('.time-input').type('8:00')
+            cy.get('.number-input').type('7')
+        })
+    })
+
+    it('should allow a user to enter the name for the reservation', () => {
+        cy
+        .get('.resy-form').within(() => {
+            cy.get('.name-input').type('Cameron')
+        })
+    })
+
+    it('should allow a user to enter the date for the reservation', () => {
+        cy
+        .get('.resy-form').within(() => {
+            cy.get('.date-input').type('01/14')
+        })
+    })
+
+    it('should allow a user to enter the time for the reservation', () => {
+        cy
+        .get('.resy-form').within(() => {
+            cy.get('.date-input').type('8:00')
+        })
+    })
+
+    it('should allow a user to enter the number of guests for the reservation', () => {
+        cy
+        .get('.resy-form').within(() => {
+            cy.get('.number-input').type('7')
+        })
+    })
+
+    it('should allow a user to create a new reservation card for their reservation', () => {
+        cy
+        .get('.resy-form').within(() => {
+            cy.get('.name-input').type('Cameron')
+            cy.get('.date-input').type('01/14')
+            cy.get('.time-input').type('8:00')
+            cy.get('.number-input').type('7')
+            cy.get('.form-button').click()
+        })
+    })
+
+    it('should display the new reservation card created', () => {
+        cy
+        .get('.resy-form').within(() => {
+            cy.get('.name-input').type('Cameron')
+            cy.get('.date-input').type('01/14')
+            cy.get('.time-input').type('8:00')
+            cy.get('.number-input').type('7')
+            cy.get('.form-button').click()
+        })
+        .get('.reservation-card:last').within(() => {
+            cy.get('h2').contains('Cameron')
+            cy.get('p').should('be.visible')
         })
     })
 
