f4e4ff5c3e426d0d69afd915421873a05b8ab144
Cameron Aragon <caragon4695@gmail.com>
2021-02-17 10:49:10 -0700

    Update Form state with handleChange method

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -12,13 +12,45 @@ class Form extends Component {
         }
     }
 
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
     render() {
         return (
             <form className='resy-form'>
-                <input type='text' placeholder='Name'></input>
-                <input type='text' placeholder='Date (mm/dd)'></input>
-                <input type='text' placeholder='Time'></input>
-                <input type='text' placeholder='Number of guests'></input>
+                <input 
+                    type='text' 
+                    placeholder='Name' 
+                    name='name'
+                    value={this.state.name}
+                    onChange={this.handleChange}>
+
+                </input>
+                <input 
+                    type='text' 
+                    placeholder='Date (mm/dd)' 
+                    name='date'
+                    value={this.state.date}
+                    onChange={this.handleChange}>
+
+                </input>
+                <input 
+                    type='text' 
+                    placeholder='Time' 
+                    name='time'
+                    value={this.state.time}
+                    onChange={this.handleChange}>
+
+                </input>
+                <input 
+                    type='text' 
+                    placeholder='Number of guests' 
+                    name='number'
+                    value={this.state.number}
+                    onChange={this.handleChange}>
+
+                </input>
                 <button className='form-button'>Make Reservation</button>
             </form>
         )
