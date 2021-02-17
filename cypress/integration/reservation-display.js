864202b6bd137ceafe209da27ac14e08c5f16e93
Cameron Aragon <caragon4695@gmail.com>
2021-02-17 11:35:49 -0700

    Add testing file for reservation display

--- cypress/integration/reservation-display.js
+++ cypress/integration/reservation-display.js
@@ -1,3 +1,43 @@
 describe('Reservation Display', () => {
-    const baseUrl = ''
+    const baseUrl = 'http://localhost:3000/?name=&date=&time=&number=';
+
+    it('should be able to visit the base and see the homepage', () => {
+        cy
+        .visit(baseUrl)
+        .location()
+    });
+
+    beforeEach(() => {
+        cy.visit(baseUrl);
+    });
+
+    it('should have a page title', () => {
+        cy
+        .get('h1')
+        .contains('Turing Cafe Reservations')
+    });
+
+    it('should have a form section with inputs and a button', () => {
+        cy
+        .get('.resy-form').within(() => {
+            cy.get('input').should('be.visible');
+            cy.get('.form-button').should('be.visible').contains('Make Reservation')
+        })
+    });
+
+    it('should display a grid of reservation cards', () => {
+        cy
+        .get('.resy-container').within(() => {
+            cy.get('.reservation-card').should('be.visible')
+        })
+    })
+
+    it('should display the guest\'s reservation information on the card', () => {
+        cy
+        .get('.reservation-card').within(() => {
+            cy.get('h2').contains('Christie')
+            cy.get('p').should('be.visible')
+            cy.get('button').should('be.visible').contains('Cancel')
+        })
+    })
 })
