9eeeb1e4af870a755be5d47ce70a7efdf9dc3f8a
Cameron Aragon <caragon4695@gmail.com>
2021-02-19 21:12:59 -0700

    Add delete functionality

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Reservations.css';
 import Card from '../Card/Card';
 
-const Reservations = ({reservations}) => {
+const Reservations = ({reservations, deleteReservation}) => {
     const reservationCards = reservations.map(reservation => {
         return (
             <Card
@@ -11,6 +11,8 @@ const Reservations = ({reservations}) => {
                 date={reservation.date}
                 time={reservation.time}
                 number={reservation.number}
+                deleteReservation={deleteReservation}
+                id={reservation.id}
             />
         )
     })
